acb805a044fb6627bfe29553014eab57df81d013
Eddie Lee <eddieberklee@gmail.com>
2016-01-10 16:00:36 -0800

    Adding Parse authentication

--- app/src/main/java/com/compscieddy/reading_logger/ReadingLoggerApplication.java
+++ app/src/main/java/com/compscieddy/reading_logger/ReadingLoggerApplication.java
@@ -2,8 +2,20 @@ package com.compscieddy.reading_logger;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
 /**
  * Created by elee on 1/10/16.
  */
 public class ReadingLoggerApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    Parse.enableLocalDatastore(this);
+    Parse.initialize(this);
+    ParseInstallation.getCurrentInstallation().saveInBackground();
+  }
 }
---
+++ app/src/main/java/com/compscieddy/reading_logger/activities/AuthenticationActivity.java
@@ -0,0 +1,15 @@
+package com.compscieddy.reading_logger.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.compscieddy.reading_logger.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_authentication);
+  }
+}
