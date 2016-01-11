7b61cd03bbae15b6653cf2af00a3b9616814cc9a
Eddie Lee <eddieberklee@gmail.com>
2016-01-10 22:01:20 -0800

    Authenticating with Parse

--- app/src/main/java/com/compscieddy/reading_logger/Util.java
+++ app/src/main/java/com/compscieddy/reading_logger/Util.java
@@ -1,8 +1,15 @@
 package com.compscieddy.reading_logger;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
 import android.graphics.PorterDuff;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by elee on 12/3/15.
@@ -21,4 +28,129 @@ public class Util {
       drawable.setColorFilter(color, mode);
     }
   }
+
+  public static void showToast(Context context, String message) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+  }
+
+  /** Time-Related */
+  public static String militaryTimeToAMPM(int militaryTime) {
+    boolean isAM = (militaryTime / 12) < 1;
+    String amPM = isAM ? "am" : "pm";
+    int adjustedTime = militaryTime % 12;
+    if (adjustedTime == 0) {
+      if (militaryTime / 12 == 1) {
+        adjustedTime = 12;
+      } else if (militaryTime / 12 == 1) {
+        adjustedTime = 24;
+      }
+    }
+    return String.valueOf(adjustedTime) + amPM;
+  }
+
+  public static int dpToPx(int dp) {
+    return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+  }
+
+  public static int pxToDp(int px) {
+    return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+  }
+
+  /** Drawing and Path Stuff */
+
+  /* If you're using this in an onDraw it may be a better idea to re-use the
+     PathMeasure object (less memory allocations which = less GC operations) */
+  /* @param distance - percentage of length as a 0-1 float */
+  public static float[] getDistanceInPathCoordinates(Path path, float distanceFraction) {
+    return getDistanceInPathCoordinates(path, distanceFraction, false);
+  }
+  public static float[] getDistanceInPathCoordinates(Path path, float distance, boolean isActualDistanceNotFraction) {
+    PathMeasure pathMeasure = new PathMeasure(path, false);
+    float[] pos = new float[2];
+    if (isActualDistanceNotFraction) {
+      pathMeasure.getPosTan(distance, pos, null);
+    } else {
+      pathMeasure.getPosTan(pathMeasure.getLength() * distance, pos, null);
+    }
+    return pos;
+  }
+  //public static float[] getDistanceInPathCoordinates(PathMeasure, Path, distance)
+
+  /** Java by default has mod do -30 % 100 = -30 instead of 70. */
+  public static float betterMod(float number, float modNumber) {
+    return (number % modNumber + modNumber) % modNumber;
+  }
+
+  public static void setPaddingLeft(View view, int padding) {
+    view.setPadding(padding, view.getPaddingTop(), view.getPaddingRight(), view.getPaddingBottom());
+  }
+  public static void setPaddingTop(View view, int padding) {
+    view.setPadding(view.getPaddingLeft(), padding, view.getPaddingRight(), view.getPaddingBottom());
+  }
+  public static void setPaddingRight(View view, int padding) {
+    view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), padding, view.getPaddingBottom());
+  }
+  public static void setPaddingBottom(View view, int padding) {
+    view.setPadding(view.getPaddingLeft(), view.getPaddingTop(), view.getPaddingRight(), padding);
+  }
+  public static void setPaddingLeftRelative(View view, int padding) {
+    view.setPaddingRelative(padding, 0, 0, 0);
+  }
+  public static void setPaddingTopRelative(View view, int padding) {
+    view.setPaddingRelative(0, padding, 0, 0);
+  }
+  public static void setPaddingRightRelative(View view, int padding) {
+    view.setPaddingRelative(0, 0, padding, 0);
+  }
+  public static void setPaddingBottomRelative(View view, int padding) {
+    view.setPaddingRelative(0, 0, 0, padding);
+  }
+
+  /**
+   * Return the color that is in-between 'startColor' and 'endColor' at 'progressPercentage'
+   * @param progressPercentage - 0.0 is beginning, 1.0 is end. Ex: 0.5 returns the color exactly
+   *                           in-between 'startColor' and 'endColor'
+   */
+  public static int getIntermediateColor(int startColor, int endColor, float progressPercentage) {
+        /* There is a more efficient way to write this with bit-shifting I think, let me (@elee) know
+           if this should be rewritten like that */
+    int startRed = Color.red(startColor);
+    int endRed = Color.red(endColor);
+    int newRed = startRed + Math.round(progressPercentage * (endRed - startRed));
+
+    int startGreen = Color.green(startColor);
+    int endGreen = Color.green(endColor);
+    int newGreen = startGreen + Math.round(progressPercentage * (endGreen - startGreen));
+
+    int startBlue = Color.blue(startColor);
+    int endBlue = Color.blue(endColor);
+    int newBlue = startBlue + Math.round(progressPercentage * (endBlue - startBlue));
+
+        /* Safety checks, just in case - could be a for loop but I didn't want to deal with storing
+           these in an array and implementing a way to log the correct variable name
+         */
+    newRed = Math.max(0, Math.min(newRed, 255));
+    newGreen = Math.max(0, Math.min(newGreen, 255));
+    newBlue = Math.max(0, Math.min(newBlue, 255));
+
+    return Color.rgb(newRed, newGreen, newBlue);
+  }
+
+  public static int setAlpha(int color, float alphaPercentage) {
+    return Color.argb(
+        Math.round(Color.alpha(color) * alphaPercentage),
+        Color.red(color),
+        Color.green(color),
+        Color.blue(color));
+  }
+
+  public static RectF constrainRect(RectF rect, float amount) {
+    RectF copyRect = new RectF(rect); // todo: don't initialize in onDraw() - GC hurts
+    copyRect.left += amount;
+    copyRect.top += amount;
+    copyRect.bottom -= amount;
+    copyRect.right -= amount;
+    return copyRect;
+  }
+
 }
--- app/src/main/java/com/compscieddy/reading_logger/activities/AuthenticationActivity.java
+++ app/src/main/java/com/compscieddy/reading_logger/activities/AuthenticationActivity.java
@@ -3,20 +3,29 @@ package com.compscieddy.reading_logger.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.compscieddy.reading_logger.R;
+import com.compscieddy.reading_logger.Util;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
-  @Bind(R.id.signup_login_button) View mSignupLoginButton;
+  private static final String TAG = AuthenticationActivity.class.getSimpleName();
+
+  @Bind(R.id.signup_login_button) View mAuthenticationButton;
   @Bind(R.id.email_input) EditText mEmailInput;
   @Bind(R.id.password_input) EditText mPasswordInput;
+  @Bind(R.id.progress_bar) ProgressBar mProgressBar;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,66 @@ public class AuthenticationActivity extends AppCompatActivity {
       startActivity(intent);
     }
 
+    mAuthenticationButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mProgressBar.setVisibility(View.VISIBLE);
+        final String email = mEmailInput.getText().toString();
+        final String password = mPasswordInput.getText().toString();
+        ParseUser.logInInBackground(email, password, new LogInCallback() {
+          @Override
+          public void done(ParseUser user, ParseException e) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            if (e == null) {
+              Intent intent = new Intent(AuthenticationActivity.this, ScrollingActivity.class);
+              intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+              startActivity(intent);
+            } else {
+              int exceptionCode = e.getCode();
+              switch (exceptionCode) {
+                case ParseException.USERNAME_TAKEN:
+                  Log.e(TAG, "Username/email exists but the password must be wrong", e);
+                  Util.showToast(AuthenticationActivity.this, "Email was recognized, but wrong password. Try again.");
+                  break;
+                case ParseException.OBJECT_NOT_FOUND:
+                case ParseException.EMAIL_NOT_FOUND:
+                  Log.d(TAG, "Username/email doesn't exist so going to try and signup the user instead.", e);
+                  signupUser(email, password);
+                  break;
+                default:
+                  Log.e(TAG, "ParseException while logging in (exception code: " + exceptionCode + ")", e);
+                  break;
+              }
+            }
+          }
+        });
+      }
+    });
 
   }
+
+  private void signupUser(String email, String password) {
+    mProgressBar.setVisibility(View.VISIBLE);
+    ParseUser newUser = new ParseUser();
+    newUser.setUsername(email);
+    newUser.setEmail(email);
+    newUser.setPassword(password);
+    newUser.signUpInBackground(new SignUpCallback() {
+      @Override
+      public void done(ParseException e) {
+        mProgressBar.setVisibility(View.INVISIBLE);
+        if (e == null) {
+          Intent intent = new Intent(AuthenticationActivity.this, ScrollingActivity.class);
+          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+          startActivity(intent);
+        } else {
+          int exceptionCode = e.getCode();
+          Log.e(TAG, "ParseException while signing up in (exception code: " + exceptionCode + ")", e);
+          Util.showToast(AuthenticationActivity.this, "Signup failed. Try again."); // TODO: be more descriptive :)
+        }
+      }
+
+    });
+  }
+
 }
