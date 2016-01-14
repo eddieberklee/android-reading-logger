f2bb57fd342438422a3c511bdc7f411e2401d139
Eddie Lee <eddieberklee@gmail.com>
2016-01-13 23:17:40 -0800

    Added an example line chart with this graph library...Idk, I'll try customizing it but....I kind wanna do my own thang.

--- app/src/main/java/com/compscieddy/reading_logger/activities/BookActivity.java
+++ app/src/main/java/com/compscieddy/reading_logger/activities/BookActivity.java
@@ -11,9 +11,15 @@ import android.widget.TextView;
 
 import com.compscieddy.reading_logger.R;
 import com.compscieddy.reading_logger.models.Book;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,6 +32,7 @@ public class BookActivity extends AppCompatActivity {
   @Bind(R.id.current_page_label) TextView mCurrentPageLabel;
   @Bind(R.id.max_page_label) TextView mMaxPageLabel;
   @Bind(R.id.of_label) TextView mOfLabel;
+  @Bind(R.id.line_chart) LineChart mLineChart;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +78,23 @@ public class BookActivity extends AppCompatActivity {
         }
       });
     }
+
+    ArrayList<Entry> valsComp1 = new ArrayList<>();
+    Entry c1e1 = new Entry(100.0f, 0);
+    valsComp1.add(c1e1);
+    Entry c1e2 = new Entry(50.0f, 2);
+    valsComp1.add(c1e2);
+
+    LineDataSet setComp1 = new LineDataSet(valsComp1, "Label 1");
+
+    ArrayList<LineDataSet> dataSets = new ArrayList<>();
+    dataSets.add(setComp1);
+
+    ArrayList<String> xVals = new ArrayList<String>();
+    xVals.add("1.Q"); xVals.add("2.Q"); xVals.add("3.Q"); xVals.add("4.Q");
+
+    LineData lineData = new LineData(xVals, dataSets);
+    mLineChart.setData(lineData);
   }
 
 }
