a2aea04508a226014bc183e280d3918cc4954851
Eddie Lee <eddieberklee@gmail.com>
2016-02-04 17:26:23 -0800

    Woo I think this is all migrated over to Firebase now - slowly getting an idea for how to organize an app with Firebase

--- app/src/main/java/com/compscieddy/reading_logger/ReadingLoggerApplication.java
+++ app/src/main/java/com/compscieddy/reading_logger/ReadingLoggerApplication.java
@@ -2,12 +2,7 @@ package com.compscieddy.reading_logger;
 
 import android.app.Application;
 
-import com.compscieddy.reading_logger.model.ParseBook;
-import com.compscieddy.reading_logger.model.ParsePageLog;
 import com.firebase.client.Firebase;
-import com.parse.Parse;
-import com.parse.ParseInstallation;
-import com.parse.ParseObject;
 
 /**
  * Created by elee on 1/10/16.
@@ -17,13 +12,6 @@ public class ReadingLoggerApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-
-    ParseObject.registerSubclass(ParseBook.class);
-    ParseObject.registerSubclass(ParsePageLog.class);
-
-    Parse.enableLocalDatastore(this);
-    Parse.initialize(this);
-    ParseInstallation.getCurrentInstallation().saveInBackground();
 
     Firebase.setAndroidContext(this);
 //    Firebase.getDefaultConfig().setLogLevel(Logger.Level.DEBUG);
